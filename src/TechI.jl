e584e93a13ecd025e06dd2278cc9cdca4c262f13
Powers, Joshua <dev@powerslabs.org>
2024-06-06 20:54:41 -0600

    added fibonnaci moving average

--- src/TechI.jl
+++ src/TechI.jl
@@ -6,7 +6,7 @@ using StatsBase
 export TP, MP, WCP, OHLC, VWAP, SD, ATR
 
 # Moving Averages
-export SMA, SMMA, EMA, WMA, HMA, KAMA, ALMA, LWMA, VWAP, TRIMA
+export SMA, SMMA, EMA, WMA, HMA, KAMA, ALMA, LWMA, VWAP, TRIMA, FMA
 
 # Oscillators
 export RSI, STOCH, MACD, CCI, ADX, BBANDS, MFI, CHAIKIN, KDJ, ICHIMOKU, WILLR, ULTOSC
